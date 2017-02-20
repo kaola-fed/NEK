c64a6d23dd6c734bdcbb74f8c102c9b6673c9c1b
Cody Chan <int64ago@gmail.com>
2017-02-20 12:49:26 +0800

    chore(moky): update to v1.7.0

--- src/commands/moky.js
+++ src/commands/moky.js
@@ -1,47 +1,9 @@
-import path from 'path';
-import { moky, parseConfig } from 'moky';
+import { builder, handler } from 'moky';
 
 exports.command = 'moky [options]';
 
 exports.desc = 'Run mock & proxy';
 
-exports.builder = {
-  e: {
-    alias: 'env',
-    demand: false,
-    default: false,
-    describe: 'Unique id mapped the project and the page configuration json object',
-  },
-  c: {
-    alias: 'config',
-    demand: false,
-    default: 'moky.config.js',
-    describe: 'Debug env, see <proxyMaps> in configure file',
-  },
-  n: {
-    alias: 'new',
-    demand: false,
-    default: false,
-    describe: 'Auto create mock file if not exists',
-  },
-  r: {
-    alias: 'rewrite',
-    demand: false,
-    default: 0,
-    describe: 'Write proxy data to mock file (1-write if not exist, 2-write even if exist)',
-  },
-  V: {
-    alias: 'verbose',
-    demand: false,
-    default: false,
-    describe: 'Show detail log',
-  },
-};
+exports.builder = builder;
 
-exports.handler = async (argv) => {
-  const { env, verbose, rewrite, new: autoGenMock } = argv;
-  const options = parseConfig(path.resolve(argv.c));
-  Object.assign(options, { env, verbose, rewrite, autoGenMock });
-
-  moky(options);
-};
+exports.handler = handler;
