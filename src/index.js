4e5bdf6204376a1715945602904810a203e1b9e1
Cody Chan <int64ago@gmail.com>
2017-03-22 00:27:38 +0800

    feat: add update notify

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
 /* eslint no-unused-expressions: 0 */
 
 const yargs = require('yargs');
+const updateNotifier = require('update-notifier');
+const pkg = require('../package');
+
+updateNotifier({ pkg }).notify();
 
 yargs
   .commandDir('commands')
